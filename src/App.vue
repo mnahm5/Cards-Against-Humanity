4f129cfc41dfccf10fecf04c02383ae1d07aaf38
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-22 08:52:51 +1000

    Made Home page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Navbar from '@/components/Navbar'
+  export default {
+    name: 'app',
+    components: {Navbar}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <div class="row">
+      <div class="col-md-offset-3 col-md-6">
+        <img class="fit" src="/static/images/home.gif"/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-offset-5 col-md-1">
+        <button class="text-center btn btn-info">Host a Game</button>
+      </div>
+      <div class="col-md-1">
+        <button class="text-center btn btn-success">Join a Game</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+
+<style scoped>
+  .fit {
+    width:100%;
+  }
+</style>
