9598485ba859c5f15ed43cfc2bb6856d71dd5ec4
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-06-26 18:43:46 +1000

    Firebase works

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="join">
     <h1>Game</h1>
+    <p v-for="question in questions">
+      {{ question.text }}
+    </p>
   </div>
 </template>
 <script>
-
+  export default {
+    props: ['questions']
+  }
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueFire from 'vuefire'
+
+Vue.use(VueFire)
 
 Vue.config.productionTip = false
 
